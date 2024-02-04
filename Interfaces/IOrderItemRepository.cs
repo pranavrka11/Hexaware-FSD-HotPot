d4e98315a9fd3e36e75f3b8bf043b63735abb084
Priyanshu-47 <85626783+Priyanshu-47@users.noreply.github.com>
2024-02-04 22:38:37 +0530

    Interfaces commit

---
+++ Interfaces/IOrderItemRepository.cs
@@ -0,0 +1,9 @@
+﻿using HotPot.Models;
+
+namespace HotPot.Interfaces
+{
+    public interface IOrderItemRepository : IRepository<(int OrderId, int MenuId), OrderItem>
+    {
+        Task<List<OrderItem>> GetAsync();
+    }
+}
