865c680d0a405bbacb7e8a2d75d15fdab12d0c35
pranavrka11 <98820851+pranavrka11@users.noreply.github.com>
2024-02-04 10:45:00 +0530

    Commit after fixing the issues

---
+++ Models/UserAddress.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotPot.Models
+{
+    public class UserAddress
+    {
+        [Key]
+        public int AddressId { get; set; }
+        public int UserId { get; set; }
+        public string? HouseNumber { get; set; }
+        public string? BuildingName { get; set; }
+        public string? Locality { get; set; }
+        public int CityId { get; set; }
+
+        [ForeignKey("CityId")]
+        public City? City { get; set; }
+        public string? LandMark { get; set; }
+
+        [ForeignKey("UserId")]
+        public User? User { get; set; }
+
+        public UserAddress()
+        {
+            
+        }
+
+        public UserAddress(int userId, string? houseNumber, string? buildingName, string? locality, int cityId, string? landMark)
+        {
+            UserId = userId;
+            HouseNumber = houseNumber;
+            BuildingName = buildingName;
+            Locality = locality;
+            CityId = cityId;
+            LandMark = landMark;
+        }
+
+        public UserAddress(int addressId, int userId, string? houseNumber, string? buildingName, string? locality, int cityId, string? landMark)
+        {
+            AddressId = addressId;
+            UserId = userId;
+            HouseNumber = houseNumber;
+            BuildingName = buildingName;
+            Locality = locality;
+            CityId = cityId;
+            LandMark = landMark;
+        }
+    }
+}
