8c44bb14d7b5a482f9b2e11e52601ea1b744a89b
pranavrka11 <98820851+pranavrka11@users.noreply.github.com>
2024-02-06 20:46:29 +0530

    DTO's for Customer login and registration

---
+++ Models/DTO/LoginUserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace HotPot.Models.DTO
+{
+    public class LoginUserDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string Token { get; set; }
+    }
+}
