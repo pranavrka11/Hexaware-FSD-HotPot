09d638684fa7cb67c9a2c9c559833fc70397f537
pranavrka11 <98820851+pranavrka11@users.noreply.github.com>
2024-02-06 20:48:16 +0530

    Mappers for Customer Registration

---
+++ Mappers/RegisterToUser.cs
@@ -0,0 +1,32 @@
+﻿using HotPot.Models;
+using HotPot.Models.DTO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HotPot.Mappers
+{
+    public class RegisterToUser
+    {
+        User user;
+
+        public RegisterToUser(RegisterCustomerDTO registerCustomer)
+        {
+            user = new User();
+            user.UserName = registerCustomer.UserName;
+            user.Role = registerCustomer.Role;
+            generatePassword(registerCustomer.Password);
+        }
+
+        void generatePassword(string  password)
+        {
+            HMACSHA512 hmac = new HMACSHA512();
+            user.Key = hmac.Key;
+            user.Password = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+        }
+
+        public User getUser()
+        {
+            return user;
+        }
+    }
+}
