931eada14ab0c4da84fc305c8e588853d3f526e0
Priyanshu-47 <85626783+Priyanshu-47@users.noreply.github.com>
2024-02-06 23:50:18 +0530

    For Login of restaurant

---
+++ Models/RestaurantOwner.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotPot.Models
+{
+    public class RestaurantOwner
+    {
+        [Key]
+        public int OwnerId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int RestaurantId { get; set; }
+        [ForeignKey("RestaurantId")]
+        public Restaurant Restaurant { get; set; }
+
+        public RestaurantOwner()
+        {
+            OwnerId = 0;
+        }
+
+        public RestaurantOwner(int ownerId, string name)
+        {
+            OwnerId = ownerId;
+            Name = name;
+        }
+
+
+    }
+}
