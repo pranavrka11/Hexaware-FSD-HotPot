1701eef31807e8660455a654e6e4d93cbc9bd8a9
Priyanshu-47 <85626783+Priyanshu-47@users.noreply.github.com>
2024-02-07 10:25:13 +0530

    Add files via upload

---
+++ Mappers/RegisterToRestaurant.cs
@@ -0,0 +1,20 @@
+﻿using HotPot.Models;
+using HotPot.Models.DTO;
+
+namespace HotPot.Mappers
+{
+    public class RegisterToRestaurant
+    {
+        RestaurantOwner restaurantOwner;
+        public RegisterToRestaurant(RegisterRestaurantDTO registerRestaurant)
+        {
+            restaurantOwner = new RestaurantOwner();
+            restaurantOwner.Name = registerRestaurant.Name;
+            restaurantOwner.UserName = registerRestaurant.UserName;
+        }
+        public RestaurantOwner GetRestaurantOwner()
+        {
+            return restaurantOwner;
+        }
+    }
+}
