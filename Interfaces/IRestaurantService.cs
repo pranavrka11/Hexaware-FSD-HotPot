5d41ec1ede1014f42e4b16963394fe51027183c7
Priyanshu-47 <85626783+Priyanshu-47@users.noreply.github.com>
2024-02-07 10:24:43 +0530

    RestoLogin

---
+++ Interfaces/IRestaurantService.cs
@@ -0,0 +1,10 @@
+﻿using HotPot.Models.DTO;
+
+namespace HotPot.Interfaces
+{
+    public interface IRestaurantService
+    {
+        public Task<LoginUserDTO> RegisterRestaurant(RegisterRestaurantDTO registerRestaurant);
+        public Task<LoginUserDTO> LogInRestaurant(LoginUserDTO loginRestaurant);
+    }
+}
