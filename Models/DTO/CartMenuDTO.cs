d0b159c63eb1281e78be4feb41222171a648d418
pranavrka11 <98820851+pranavrka11@users.noreply.github.com>
2024-02-13 12:52:03 +0530

    Some other DTOs

---
+++ Models/DTO/CartMenuDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace HotPot.Models.DTO
+{
+    public class CartMenuDTO
+    {
+        public int CartId { get; set; }
+        public int CustomerId { get; set; }
+        public int RestaurantId { get; set; }
+        public int MenuItemId { get; set; }
+        public string MenuTitle { get; set; }
+        public int Quantity { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ Models/DTO/OrderMenuDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace HotPot.Models.DTO
+{
+    public class OrderMenuDTO
+    {
+        public int orderId { get; set; }
+        public int customerId { get; set; }
+        public int restaurantId { get; set; }
+        public List<MenuNameDTO> menuName { get; set; }
+        public float Price { get; set; }
+        public string Status { get; set; }
+    }
+}
