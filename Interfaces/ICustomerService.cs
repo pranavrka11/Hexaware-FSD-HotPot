1ede0bd1928d14d9bc017bf1ab8f60839e0b2936
Priyanshu-47 <85626783+Priyanshu-47@users.noreply.github.com>
2024-02-14 15:04:28 +0530

    Add files via upload

---
+++ Interfaces/ICustomerService.cs
@@ -0,0 +1,10 @@
+﻿using HotPot.Models.DTO;
+
+namespace HotPot.Interfaces
+{
+    public interface ICustomerService
+    {
+        public Task<LoginUserDTO> RegisterCustomer(RegisterCustomerDTO registerCustomer);
+        public Task<LoginUserDTO> LogIn(LoginUserDTO loginCustomer);
+    }
+}
---
+++ Interfaces/ICustomerUserService.cs
@@ -0,0 +1,27 @@
+﻿using HotPot.Models;
+using HotPot.Models.DTO;
+
+namespace HotPot.Interfaces
+{
+    public interface ICustomerUserService
+    {
+        //Address Layer
+        public Task<CustomerAddress> AddCustomerAddress(CustomerAddress customerAddress);
+        public Task<CustomerAddress> UpdateCustomerAddress(int addressId, CustomerAddressUpdateDTO addressUpdateDto);
+        public Task<CustomerAddress> ViewCustomerAddressByCustomerId(int customerId);
+
+        //Review Layer
+        public Task<CustomerReview> AddCustomerReview(CustomerReview customerReview);
+        public Task<CustomerReview> ViewCustomerReview(int customerReviewId);
+        public Task<CustomerReview> UpdateCustomerReviewText(CustomerReviewUpdateDTO reviewUpdateDTO);
+        public Task<CustomerReview> DeleteCustomerReview(int customerReviewId);
+
+        //Menu Layer 
+        public Task<List<Menu>> ViewMenu();
+        public Task<List<Menu>> SearchMenu(string query);
+        public Task<List<Menu>> FilterMenuByPriceRange(float minPrice, float maxPrice);
+        public Task<List<Menu>> FilterMenuByType(string type);
+        public Task<List<Menu>> FilterMenuByRestaurant(int restaurantId);
+        public Task<List<Menu>> FilterMenuByCuisine(string cuisine);
+    }
+}
