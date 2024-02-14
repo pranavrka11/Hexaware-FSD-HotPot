e659c76b6e64e60a5b2bf3d96b105b3f016da74f
Priyanshu-47 <85626783+Priyanshu-47@users.noreply.github.com>
2024-02-14 14:59:14 +0530

    Add files via upload

--- Context/HotpotContext.cs
+++ Context/HotpotContext.cs
@@ -16,20 +16,29 @@ namespace HotPot.Context
         public DbSet<Menu>? Menus { get; set; }
         public DbSet<NutritionalInfo>? NutritionalInfos { get; set;}
         public DbSet<Order>? Orders { get; set; }
-        public DbSet<OrderItem>? OrdersItems { get; set;}
+        public DbSet<OrderItem>? OrderItems { get; set;}
         public DbSet<Payment>? Payments { get; set; }
         public DbSet<Restaurant>? Restaurants { get; set; } 
-        public DbSet<RestaurantSpeciality>? RestaurantsSpecialities { get; set; }
+        public DbSet<RestaurantSpeciality>? RestaurantSpecialities { get; set; }
         public DbSet<State>? States { get; set; }
+        public DbSet<Customer>? Customers { get; set; }
+        public DbSet<CustomerAddress>? CustomerAddresses { get; set; }
+        public DbSet<CustomerReview>? CustomerReviews { get; set; }
+        public DbSet<RestaurantOwner>? RestaurantOwners { get; set; }
         public DbSet<User>? Users { get; set; }
-        public DbSet<UserAddress>? UserAddresses { get; set; }
-        public DbSet<UserReviews>? UserReviews { get; set; }
+        public DbSet<Cart>? Carts { get; set; }
 
+        /// <summary>
+        /// Configures the model for the database context, specifically handling composite keys for OrderItem entities.
+        /// </summary>
+        /// <param name="modelBuilder">The builder for configuring the model.</param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Configure composite key for OrderItem entity
             modelBuilder.Entity<OrderItem>()
                 .HasKey(o => new { o.OrderId, o.MenuId });
 
+            // Configure relationships for OrderItem entity
             modelBuilder.Entity<OrderItem>()
                 .HasOne(oi => oi.Order)
                 .WithMany(o => o.OrderItems)
@@ -40,6 +49,13 @@ namespace HotPot.Context
                 .HasOne(oi => oi.Menu)
                 .WithMany(m => m.OrderItems)
                 .HasForeignKey(oi => oi.MenuId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            //configure relationship for the 
+            modelBuilder.Entity<Cart>()
+                .HasOne(c => c.Restaurant)
+                .WithMany()
+                .HasForeignKey(c => c.RestaurantId)
                 .OnDelete(DeleteBehavior.NoAction);
         }
     }
