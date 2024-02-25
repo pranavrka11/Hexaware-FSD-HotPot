d73893b754c061b24166519ae32f6c1b1d3299cf
Priyanshu-47 <85626783+Priyanshu-47@users.noreply.github.com>
2024-02-25 15:46:06 +0530

    css updated

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import './MenuActual.css'
 
 function Menu() {
     const [menuItems, setMenuItems] = useState([]);
--- src/Components/RestaurantOwner/Registration.js
+++ src/Components/RestaurantOwner/Registration.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './RestaurantOwner.css';
+// import './RestaurantOwner.css';
+import './Registration.css';
 
 function Registration() {
   const [isLoginFormDisplayed, setIsLoginFormDisplayed] = useState(false);
