e0337225b2628334856364fb94d029518eae2ca8
Priyanshu-47 <85626783+Priyanshu-47@users.noreply.github.com>
2024-06-08 16:38:30 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,47 +1,78 @@
 // import logo from './logo.svg';
 import './App.css';
-import RegistrationHeader from './Components/RestaurantOwner/RegistrationHeader';
-import Registration from './Components/RestaurantOwner/Registration';
-import RegistrationFooter from './Components/RestaurantOwner/RegistrationFooter';
-import CustomerRegistration from './Components/Customer/CustomerRegistration';
-import MenuHeader from './Components/Menu/MenuHeader';
-import MenuCategories from './Components/Menu/MenuCategories';
-import Menu from './Components/Menu/Menu';
-import Profile from './Components/Profile/Profile';
-import OwnerProfile from './Components/OwnerProfile/OwnerProfile';
-import Checkout from './Components/Checkout/Checkout';
-import Confirmation from './Components/Confirmation/Confirmation';
-import Admin from './Components/Admin/Admin';
-
+import HomePage from './Components/Homepage/HomePage';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import RestaurantPage from './Components/RestaurantsPage/RestaurantPage';
+import AboutPage from './Components/Aboutpage/AboutPage';
+import Footer from './Components/Footer';
+import Cartitems from './Components/Cart/Cartitems';
+import { useEffect, useState } from 'react';
+import { AuthProvider } from './Context/AuthContext';
+import CustomerRegistration from './Components/Loginpage/CustomerRegistration';
+import MenuPage from './Components/Menupage/MenuPage';
+import CustomerProfile from './Components/CustomerProfilepage/CustomerProfile';
+import OwnerProfile from './Components/RestaurantOwnerProfilepage/OwnerProfile';
+import PartnerProfile from './Components/DeliveryPartnerProfilepage/PartnerProfile';
+import Registration from './Components/Loginpage/RestaurantLoginPage';
+import DeliveryPartnerLogin from './Components/Loginpage/DeliveryPartnerLoginPage';
+import Checkout from './Components/Checkoutpage/CheckOutPage';
+import Confirmation from './Components/Confirmationpage/Confirmation';
+import AdminLogin from './Components/Loginpage/AdminLoginPage';
+import Admin from './Components/AdminPage/Admin';
+import Contact from './Components/Contactpage/Contact';
 
 
 
 function App() {
+  var [isLoggedIn, setIsLoggedIn] = useState(false);
+  // sessionStorage.setItem('selectedCity', 'Pune');
+
+  useEffect(() => {
+    const token = sessionStorage.getItem('Token');
+    setIsLoggedIn(!!token);
+  }, []);
+
+  var handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const userId = sessionStorage.getItem('UserId');
+
   return (
-    <div className="App">
-      <Admin />
-      
-      {/* <RegistrationHeader />
-      <Registration />
-      <RegistrationFooter /> */}
-      
-      {/* <RegistrationHeader />
-      <CustomerRegistration />
-      <RegistrationFooter /> */}
-
-      {/* <MenuHeader/> */}
-      {/* <MenuCategories /> */}
-      {/* <Menu /> */}
-      {/* <RegistrationFooter /> */}
-
-      {/* <Profile /> */}
-
-      {/* <OwnerProfile /> */}
-
-      {/* <Checkout /> */}
-
-      {/* <Confirmation /> */}
-    </div>
+    <>
+      <AuthProvider>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/restaurants' element={<RestaurantPage />} />
+          {/* <Route path='/restaurants' element={<RestaurantPage />} /> */}
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/carts'>
+            <Route path=':userId' element={<Cartitems />} />
+          </Route>
+          {/* <Route path={'/carts/:userId'} element={<Cartitems />} /> */}
+          <Route path='/customer-login' element={<CustomerRegistration />} />
+          <Route path='/menus'>
+            <Route path=':restaurantId' element={<MenuPage />} />
+          </Route>
+          <Route path='/customer-profile'>
+            <Route path=':customerId' element={<CustomerProfile />} />
+          </Route>
+          {/* <Route path='/customer-login' element={<Login />} /> */}
+          <Route path='/restaurant-login' element={<Registration />} />
+          <Route path='/restaurant-profile' element={<OwnerProfile />} />
+          <Route path='/delivery-partner-login' element={<DeliveryPartnerLogin />} />
+          <Route path='/delivery-partner-profile' element={<PartnerProfile />} />
+          <Route path='/check-out-page' element={<Checkout />} />
+          <Route path='/order-confirmation' element={<Confirmation />} />
+          <Route path='/admin-login' element={<AdminLogin />} />
+          <Route path='/admin-profile' element={<Admin />} />
+        </Routes>
+        <Footer />
+      </AuthProvider>
+    </>
   );
 }
 
